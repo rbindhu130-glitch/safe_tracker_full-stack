8c93b5b5941015359d2acf008d6059da9de145d1
Indhu Ramar <inramar@fssa.freshworks.com>
2026-06-07 14:10:21 +0530

    Fix: Truncate password to 72 bytes to avoid bcrypt error

--- api/router.py
+++ api/router.py
@@ -13,10 +13,10 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt", "pbkdf2_sha256"], deprecated="auto")
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
